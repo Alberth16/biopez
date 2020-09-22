21605a24853baa841aa29986f756ad40f7866867
Alberth16 <albherth5@gmail.com>
2020-09-21 22:55:28 -0600

    Arreglo de iframe

--- pgs/menu.php
+++ pgs/menu.php
@@ -14,31 +14,30 @@
     <?php include "../incl/header.php"?>
     
     
-        <h2>Menu Principal</h2>
+        <h2 id="Nombre_Empresa"></h2>
         <div class="datos">
 
             <nav class="topnav" id="myTopnav">
-                <a ref="Indicadores.php" target="Contenido" class="dropdown"><i class="far fa-chart-bar"></i>  Indicadores de Consumo</a>
+                <a ref="Indicadores.php" target="Contenido" class="dropdown"  onclick="myFunction();"><i class="far fa-chart-bar"></i>  Indicadores de Consumo</a>
                 <div class="dropdown">
                     <button class="dropbtn"><i class="fas fa-book"></i>    Reportes de proceso <i class="fa fa-caret-down"></i></button>
                     <div class="dropdown-content">
-                        <a href="#" id="btnsiembra"><i class="fas fa-database"></i>    Curva de crecimiento</a>
-                        <a href="#" id="btnlecturas"><i class="far fa-chart-bar"></i>  Ventas</a>
+                        <a href="#" target="Contenido" id="btnsiembra" onclick="myFunction();"><i class="fas fa-database"></i>    Curva de crecimiento</a>
+                        <a href="#" target="Contenido" id="btnlecturas" onclick="myFunction();"><i class="far fa-chart-bar"></i>  Descargar Datos</a>
                     </div>
                 </div>
                 <div class="dropdown">
                     <button class="dropbtn"><i class="fas fa-database"></i>    Registro de Datos <i class="fa fa-caret-down"></i></button>
                     <div class="dropdown-content">
-                        <a href="Compras.php" target="Contenido" id="btncompras"><i class="far fa-money-bill-alt"></i>    Compras </a>
-                        <a href="Consumos.php" target="Contenido" id="btnconsumos"><i class="far fa-chart-bar"></i>  Consumos</a>
-                        <a href="Siembra.php" target="Contenido" id="btnsiembra"><i class="fas fa-book"></i>    Siembra</a>
-                        <a href="Monitoreo.php" target="Contenido" id="btnmonitoreo"><i class="fas fa-book"></i>    Monitoreo</a>
-                        <a href="Biometrico.php" target="Contenido"  id="btnbiometrico"><i class="fas fa-database"></i>    Biometrico</a>
+                        <a href="Compras.php" target="Contenido" id="btncompras" onclick="myFunction();"><i class="far fa-money-bill-alt"></i>    Compras </a>
+                        <a href="Consumos.php" target="Contenido" id="btnconsumos" onclick="myFunction();"><i class="far fa-chart-bar"></i>  Consumos</a>
+                        <a href="Siembra.php" target="Contenido" id="btnsiembra" onclick="myFunction();"><i class="fas fa-book"></i>    Siembra</a>
+                        <a href="Monitoreo.php" target="Contenido" id="btnmonitoreo" onclick="myFunction();"><i class="fas fa-book"></i>    Monitoreo</a>
+                        <a href="Biometrico.php" target="Contenido"  id="btnbiometrico" onclick="myFunction();"><i class="fas fa-database"></i>    Biometrico</a>
                     </div>
                 </div>
 
-                <!-- <a href="javsascript:void(0);" class="icon" onclick="cambiaClase();"><i class="fa fa-bars"></i></a> -->
-                <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+                <a href="javsascript:void(0);" class="icon" onclick="myFunction();"><i class="fa fa-bars"></i></a>
             </nav>
 
 
@@ -46,7 +45,7 @@
 
 
             <div class="contenedor">
-                <iframe src="Indicadores.php" name="Contenido" frameborder="0" width="100%" height="100%"></iframe>
+                <iframe src="Indicadores.php" name="Contenido" frameborder="1" width="100%" height="100%" ></iframe>
             </div>
         </div>
         <div class="container transp80 footer">
@@ -58,8 +57,8 @@
             FechaActual();
             HoraActual();
         }
-        $(document).on('click', '.btnregistros', function() {
-            window.location='Menu_Registro.php';
+        $(document).on('click', '#btncancel', function() {
+            window.location='../php/logout.php';
         });
     </script>
 </body>
